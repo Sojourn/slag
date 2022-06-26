910131e5f093d1db28df9ac059e93618c4ce2a48
Joel Rausch <jsrausch.sojourn@gmail.com>
2022-06-26 13:45:32 -0500

    First cut at a ResourceContext class

---
+++ include/slag/resource_context.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <bitset>
+#include <vector>
+#include "slag/operation.h"
+#include "slag/file_descriptor.h"
+
+namespace slag {
+
+    class Resource;
+    class Operation;
+
+    class ResourceContext {
+    public:
+        explicit ResourceContext(Resource& resource);
+        ResourceContext(ResourceContext&&) noexcept = delete;
+        ResourceContext(const ResourceContext&) = delete;
+        ~ResourceContext();
+
+        ResourceContext& operator=(ResourceContext&&) noexcept = delete;
+        ResourceContext& operator=(const ResourceContext&) = delete;
+
+    private:
+        friend class Driver;
+
+        [[nodiscard]] bool has_resource() const;
+        [[nodiscard]] Resource& resource();
+        [[nodiscard]] const Resource& resource() const;
+        void remove_resource();
+        void update_resource(Resource& resource);
+
+        [[nodiscard]] bool has_deferred_action(OperationAction operation_action) const;
+        void set_deferred_action(OperationAction operation_action);
+        void reset_deferred_action(OperationAction operation_action);
+        bool update_deferred_action(OperationAction operation_action);
+
+        [[nodiscard]] std::vector<Operation*>& operations();
+        [[nodiscard]] const std::vector<Operation*>& operations() const;
+
+        [[nodiscard]] FileDescriptor& file_descriptor();
+        [[nodiscard]] const FileDescriptor& file_descriptor() const;
+
+        [[nodiscard]] bool is_referenced() const;
+
+    private:
+        Resource*                           resource_;
+        std::bitset<OPERATION_ACTION_COUNT> deferred_actions_;
+        std::vector<Operation*>             operations_;
+        FileDescriptor                      file_descriptor_;
+    };
+
+}
