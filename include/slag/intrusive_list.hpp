82e833248e2e693e653acbc0d78db4ba7021efc8
Joel Rausch <jsrausch.sojourn@gmail.com>
2023-02-12 09:00:18 -0600

    Move function definitions into the namespace to reduce visual noise

--- include/slag/intrusive_list.hpp
+++ include/slag/intrusive_list.hpp
@@ -3,40 +3,21 @@
 #include <cassert>
 #include <cstdlib>
 
-inline slag::IntrusiveListNode::IntrusiveListNode()
-    : prev_{this}
-    , next_{this}
-{
-}
+namespace slag {
 
-inline slag::IntrusiveListNode::IntrusiveListNode(IntrusiveListNode&& other) noexcept
-    : prev_{this}
-    , next_{this}
-{
-    if (other.is_linked()) {
-        prev_ = std::exchange(other.prev_, &other);
-        next_ = std::exchange(other.next_, &other);
-
-        prev_->next_ = this;
-        next_->prev_ = this;
-
-        assert(is_linked());
+    inline IntrusiveListNode::IntrusiveListNode()
+        : prev_{this}
+        , next_{this}
+    {
     }
 
-    assert(!other.is_linked());
-}
-
-inline slag::IntrusiveListNode::~IntrusiveListNode() {
-    unlink();
-}
-
-inline slag::IntrusiveListNode& slag::IntrusiveListNode::operator=(IntrusiveListNode&& that) noexcept {
-    if (this != &that) {
-        unlink();
-
-        if (that.is_linked()) {
-            prev_ = std::exchange(that.prev_, &that);
-            next_ = std::exchange(that.next_, &that);
+    inline IntrusiveListNode::IntrusiveListNode(IntrusiveListNode&& other) noexcept
+        : prev_{this}
+        , next_{this}
+    {
+        if (other.is_linked()) {
+            prev_ = std::exchange(other.prev_, &other);
+            next_ = std::exchange(other.next_, &other);
 
             prev_->next_ = this;
             next_->prev_ = this;
@@ -44,241 +25,264 @@ inline slag::IntrusiveListNode& slag::IntrusiveListNode::operator=(IntrusiveList
             assert(is_linked());
         }
 
-        assert(!that.is_linked());
+        assert(!other.is_linked());
     }
 
-    return *this;
-}
-
-inline bool slag::IntrusiveListNode::is_linked() const {
-    return prev_ != this; // not self linked
-}
-
-inline void slag::IntrusiveListNode::unlink() {
-    if (is_linked()) {
-        auto prev = std::exchange(prev_, this);
-        auto next = std::exchange(next_, this);
-
-        prev->next_ = next;
-        next->prev_ = prev;
+    inline IntrusiveListNode::~IntrusiveListNode() {
+        unlink();
     }
 
-    assert(!is_linked());
-}
+    inline IntrusiveListNode& IntrusiveListNode::operator=(IntrusiveListNode&& that) noexcept {
+        if (this != &that) {
+            unlink();
 
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline T& slag::IntrusiveListNode::from_node(IntrusiveListNode& node) noexcept {
-    static const ptrdiff_t node_offset = reinterpret_cast<ptrdiff_t>(
-        &to_node<T, node_>(*reinterpret_cast<T*>(NULL))
-    );
+            if (that.is_linked()) {
+                prev_ = std::exchange(that.prev_, &that);
+                next_ = std::exchange(that.next_, &that);
 
-    return *reinterpret_cast<T*>(reinterpret_cast<std::byte*>(&node) - node_offset);
-}
+                prev_->next_ = this;
+                next_->prev_ = this;
 
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline slag::IntrusiveListNode& slag::IntrusiveListNode::to_node(T& element) noexcept {
-    return element.*node_;
-}
+                assert(is_linked());
+            }
 
-inline void slag::IntrusiveListNode::link_before(IntrusiveListNode& other) noexcept {
-    assert(this != &other);
-    if (other.is_linked()) {
-        assert(other.prev_->is_linked());
-        assert(other.next_->is_linked());
+            assert(!that.is_linked());
+        }
+
+        return *this;
     }
 
-    if (is_linked()) {
-        abort(); // throw instead?
+    inline bool IntrusiveListNode::is_linked() const {
+        return prev_ != this; // not self linked
     }
 
-    IntrusiveListNode* prev = other.prev_;
-    IntrusiveListNode* next = &other;
+    inline void IntrusiveListNode::unlink() {
+        if (is_linked()) {
+            auto prev = std::exchange(prev_, this);
+            auto next = std::exchange(next_, this);
 
-    prev_ = prev;
-    next_ = next;
+            prev->next_ = next;
+            next->prev_ = prev;
+        }
 
-    prev_->next_ = this;
-    next_->prev_ = this;
-
-    assert(prev->is_linked());
-    assert(is_linked());
-    assert(next->is_linked());
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline slag::IntrusiveListIterator<T, node_>::IntrusiveListIterator()
-    : current_node_{nullptr}
-{
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline slag::IntrusiveListIterator<T, node_>::IntrusiveListIterator(IntrusiveListNode& node)
-    : current_node_{&node}
-{
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline auto slag::IntrusiveListIterator<T, node_>::operator*() -> reference {
-    return IntrusiveListNode::from_node<T, node_>(*current_node_);
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline auto slag::IntrusiveListIterator<T, node_>::operator->() -> pointer {
-    return &operator*();
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline slag::IntrusiveListIterator<T, node_>& slag::IntrusiveListIterator<T, node_>::operator++() {
-    current_node_ = current_node_->next_;
-    return *this;
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline slag::IntrusiveListIterator<T, node_>& slag::IntrusiveListIterator<T, node_>::operator--() {
-    current_node_ = current_node_->prev_;
-    return *this;
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline slag::IntrusiveListIterator<T, node_> slag::IntrusiveListIterator<T, node_>::operator++(int) {
-    IntrusiveListIterator result = *this;
-    current_node_ = current_node_->next_;
-    return result;
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline slag::IntrusiveListIterator<T, node_> slag::IntrusiveListIterator<T, node_>::operator--(int) {
-    IntrusiveListIterator result = *this;
-    current_node_ = current_node_->prev_;
-    return result;
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline bool slag::IntrusiveListIterator<T, node_>::operator==(const IntrusiveListIterator& that) const {
-    return current_node_ == that.current_node_;
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline bool slag::IntrusiveListIterator<T, node_>::operator!=(const IntrusiveListIterator& that) const {
-    return !operator==(that);
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline slag::IntrusiveList<T, node_>::IntrusiveList(IntrusiveList&& other) noexcept
-    : root_{std::move(other.root_)}
-{
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline slag::IntrusiveList<T, node_>::~IntrusiveList() {
-    while (!is_empty()) {
-        erase(front());
-    }
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline slag::IntrusiveList<T, node_>& slag::IntrusiveList<T, node_>::operator=(IntrusiveList&& that) noexcept {
-    if (this != &that) {
-        clear();
-
-        root_ = std::move(that.root_);
+        assert(!is_linked());
     }
 
-    return *this;
-}
+    template<typename T, IntrusiveListNode T::*node_>
+    inline T& IntrusiveListNode::from_node(IntrusiveListNode& node) noexcept {
+        static const ptrdiff_t node_offset = reinterpret_cast<ptrdiff_t>(
+            &to_node<T, node_>(*reinterpret_cast<T*>(NULL))
+        );
 
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline bool slag::IntrusiveList<T, node_>::is_empty() const {
-    return !root_.is_linked();
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline size_t slag::IntrusiveList<T, node_>::size() const {
-    // TODO: add const_iterator begin/end overloads to avoid this cast
-    auto self = const_cast<IntrusiveList<T, node_>*>(this);
-    return std::distance(self->begin(), self->end());
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline auto slag::IntrusiveList<T, node_>::begin() -> iterator {
-    return iterator{*root_.next_};
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline auto slag::IntrusiveList<T, node_>::end() -> iterator {
-    return iterator{root_};
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline T& slag::IntrusiveList<T, node_>::front() {
-    if (is_empty()) {
-        abort(); // throw instead?
+        return *reinterpret_cast<T*>(reinterpret_cast<std::byte*>(&node) - node_offset);
     }
 
-    return IntrusiveListNode::from_node<T, node_>(*root_.next_);
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline T& slag::IntrusiveList<T, node_>::back() {
-    if (is_empty()) {
-        abort(); // throw instead?
+    template<typename T, IntrusiveListNode T::*node_>
+    inline IntrusiveListNode& IntrusiveListNode::to_node(T& element) noexcept {
+        return element.*node_;
     }
 
-    return IntrusiveListNode::from_node<T, node_>(*root_.prev_);
-}
+    inline void IntrusiveListNode::link_before(IntrusiveListNode& other) noexcept {
+        assert(this != &other);
+        if (other.is_linked()) {
+            assert(other.prev_->is_linked());
+            assert(other.next_->is_linked());
+        }
 
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline void slag::IntrusiveList<T, node_>::push_front(T& element) {
-    IntrusiveListNode& node = IntrusiveListNode::to_node<T, node_>(element);
-    assert(!node.is_linked());
-    node.link_before(*root_.next_);
-}
+        if (is_linked()) {
+            abort(); // throw instead?
+        }
 
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline void slag::IntrusiveList<T, node_>::push_back(T& element) {
-    IntrusiveListNode& node = IntrusiveListNode::to_node<T, node_>(element);
-    assert(!node.is_linked());
-    node.link_before(root_);
-}
+        IntrusiveListNode* prev = other.prev_;
+        IntrusiveListNode* next = &other;
 
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline T& slag::IntrusiveList<T, node_>::pop_front() {
-    T& element = front();
-    erase(element);
-    return element;
-}
+        prev_ = prev;
+        next_ = next;
 
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline T& slag::IntrusiveList<T, node_>::pop_back() {
-    T& element = back();
-    erase(element);
-    return element;
-}
+        prev_->next_ = this;
+        next_->prev_ = this;
 
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline void slag::IntrusiveList<T, node_>::erase(T& element) {
-    IntrusiveListNode& node = IntrusiveListNode::to_node<T, node_>(element);
-    assert(node.is_linked());
-    node.unlink();
-}
+        assert(prev->is_linked());
+        assert(is_linked());
+        assert(next->is_linked());
+    }
 
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline auto slag::IntrusiveList<T, node_>::erase(iterator it) -> iterator {
-    if (it == end()) {
+    template<typename T, IntrusiveListNode T::*node_>
+    inline IntrusiveListIterator<T, node_>::IntrusiveListIterator()
+        : current_node_{nullptr}
+    {
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline IntrusiveListIterator<T, node_>::IntrusiveListIterator(IntrusiveListNode& node)
+        : current_node_{&node}
+    {
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline auto IntrusiveListIterator<T, node_>::operator*() -> reference {
+        return IntrusiveListNode::from_node<T, node_>(*current_node_);
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline auto IntrusiveListIterator<T, node_>::operator->() -> pointer {
+        return &operator*();
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline IntrusiveListIterator<T, node_>& IntrusiveListIterator<T, node_>::operator++() {
+        current_node_ = current_node_->next_;
+        return *this;
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline IntrusiveListIterator<T, node_>& IntrusiveListIterator<T, node_>::operator--() {
+        current_node_ = current_node_->prev_;
+        return *this;
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline IntrusiveListIterator<T, node_> IntrusiveListIterator<T, node_>::operator++(int) {
+        IntrusiveListIterator result = *this;
+        current_node_ = current_node_->next_;
+        return result;
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline IntrusiveListIterator<T, node_> IntrusiveListIterator<T, node_>::operator--(int) {
+        IntrusiveListIterator result = *this;
+        current_node_ = current_node_->prev_;
+        return result;
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline bool IntrusiveListIterator<T, node_>::operator==(const IntrusiveListIterator& that) const {
+        return current_node_ == that.current_node_;
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline bool IntrusiveListIterator<T, node_>::operator!=(const IntrusiveListIterator& that) const {
+        return !operator==(that);
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline IntrusiveList<T, node_>::IntrusiveList(IntrusiveList&& other) noexcept
+        : root_{std::move(other.root_)}
+    {
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline IntrusiveList<T, node_>::~IntrusiveList() {
+        while (!is_empty()) {
+            erase(front());
+        }
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline IntrusiveList<T, node_>& IntrusiveList<T, node_>::operator=(IntrusiveList&& that) noexcept {
+        if (this != &that) {
+            clear();
+
+            root_ = std::move(that.root_);
+        }
+
+        return *this;
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline bool IntrusiveList<T, node_>::is_empty() const {
+        return !root_.is_linked();
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline size_t IntrusiveList<T, node_>::size() const {
+        // TODO: add const_iterator begin/end overloads to avoid this cast
+        auto self = const_cast<IntrusiveList<T, node_>*>(this);
+        return std::distance(self->begin(), self->end());
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline auto IntrusiveList<T, node_>::begin() -> iterator {
+        return iterator{*root_.next_};
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline auto IntrusiveList<T, node_>::end() -> iterator {
+        return iterator{root_};
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline T& IntrusiveList<T, node_>::front() {
+        if (is_empty()) {
+            abort(); // throw instead?
+        }
+
+        return IntrusiveListNode::from_node<T, node_>(*root_.next_);
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline T& IntrusiveList<T, node_>::back() {
+        if (is_empty()) {
+            abort(); // throw instead?
+        }
+
+        return IntrusiveListNode::from_node<T, node_>(*root_.prev_);
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline void IntrusiveList<T, node_>::push_front(T& element) {
+        IntrusiveListNode& node = IntrusiveListNode::to_node<T, node_>(element);
+        assert(!node.is_linked());
+        node.link_before(*root_.next_);
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline void IntrusiveList<T, node_>::push_back(T& element) {
+        IntrusiveListNode& node = IntrusiveListNode::to_node<T, node_>(element);
+        assert(!node.is_linked());
+        node.link_before(root_);
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline T& IntrusiveList<T, node_>::pop_front() {
+        T& element = front();
+        erase(element);
+        return element;
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline T& IntrusiveList<T, node_>::pop_back() {
+        T& element = back();
+        erase(element);
+        return element;
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline void IntrusiveList<T, node_>::erase(T& element) {
+        IntrusiveListNode& node = IntrusiveListNode::to_node<T, node_>(element);
+        assert(node.is_linked());
+        node.unlink();
+    }
+
+    template<typename T, IntrusiveListNode T::*node_>
+    inline auto IntrusiveList<T, node_>::erase(iterator it) -> iterator {
+        if (it == end()) {
+            return it;
+        }
+
+        T& element = *it;
+        ++it;
+
+        erase(element);
+
         return it;
     }
 
-    T& element = *it;
-    ++it;
-
-    erase(element);
-
-    return it;
-}
-
-template<typename T, slag::IntrusiveListNode T::*node_>
-inline void slag::IntrusiveList<T, node_>::clear() {
-    while (!is_empty()) {
-        erase(front());
+    template<typename T, IntrusiveListNode T::*node_>
+    inline void IntrusiveList<T, node_>::clear() {
+        while (!is_empty()) {
+            erase(front());
+        }
     }
+
 }
