98fc3d95bb7d9fd58965d6e354077635b8be95b6
Joel Rausch <jsrausch.sojourn@gmail.com>
2023-06-18 11:54:21 -0500

    Add non-const visit overloads

--- include/slag/visit.h
+++ include/slag/visit.h
@@ -15,22 +15,43 @@
 namespace slag {
 
     template<typename Visitor, typename T>
+    constexpr void visit(Visitor&& visitor, T& value);
+
+    template<typename Visitor, typename T>
     constexpr void visit(Visitor&& visitor, const T& value);
+
+    template<typename Visitor, typename T>
+    constexpr void visit(Visitor&& visitor, std::optional<T>& value);
 
     template<typename Visitor, typename T>
     constexpr void visit(Visitor&& visitor, const std::optional<T>& value);
 
     template<typename Visitor, typename T1, typename T2>
+    constexpr void visit(Visitor&& visitor, std::pair<T1, T2>& value);
+
+    template<typename Visitor, typename T1, typename T2>
     constexpr void visit(Visitor&& visitor, const std::pair<T1, T2>& value);
+
+    template<typename Visitor, typename... Types>
+    constexpr void visit(Visitor&& visitor, std::tuple<Types...>& value);
 
     template<typename Visitor, typename... Types>
     constexpr void visit(Visitor&& visitor, const std::tuple<Types...>& value);
 
     template<typename Visitor, typename... Types>
+    constexpr void visit(Visitor&& visitor, std::variant<Types...>& value);
+
+    template<typename Visitor, typename... Types>
     constexpr void visit(Visitor&& visitor, const std::variant<Types...>& value);
 
     template<typename Visitor, typename T>
+    constexpr void visit(Visitor&& visitor, std::vector<T>& value);
+
+    template<typename Visitor, typename T>
     constexpr void visit(Visitor&& visitor, const std::vector<T>& value);
+
+    template<typename Visitor, typename Key, typename T>
+    constexpr void visit(Visitor&& visitor, std::unordered_map<Key, T>& value);
 
     template<typename Visitor, typename Key, typename T>
     constexpr void visit(Visitor&& visitor, const std::unordered_map<Key, T>& value);
--- include/slag/visit.hpp
+++ include/slag/visit.hpp
@@ -3,8 +3,23 @@
 namespace slag {
 
     template<typename Visitor, typename T>
+    constexpr inline void visit(Visitor&& visitor, T& value) {
+        visitor.enter(value);
+        visitor.leave(value);
+    }
+
+    template<typename Visitor, typename T>
     constexpr inline void visit(Visitor&& visitor, const T& value) {
         visitor.enter(value);
+        visitor.leave(value);
+    }
+
+    template<typename Visitor, typename T>
+    constexpr inline void visit(Visitor&& visitor, std::optional<T>& value) {
+        visitor.enter(value);
+        if (value) {
+            visit(visitor, *value);
+        }
         visitor.leave(value);
     }
 
@@ -18,11 +33,34 @@ namespace slag {
     }
 
     template<typename Visitor, typename T1, typename T2>
+    constexpr inline void visit(Visitor&& visitor, std::pair<T1, T2>& value) {
+        visitor.enter(value);
+        {
+            visit(visitor, value.first);
+            visit(visitor, value.second);
+        }
+        visitor.leave(value);
+    }
+
+    template<typename Visitor, typename T1, typename T2>
     constexpr inline void visit(Visitor&& visitor, const std::pair<T1, T2>& value) {
         visitor.enter(value);
         {
             visit(visitor, value.first);
             visit(visitor, value.second);
+        }
+        visitor.leave(value);
+    }
+
+    template<typename Visitor, typename... Types>
+    constexpr inline void visit(Visitor&& visitor, std::tuple<Types...>& value) {
+        visitor.enter(value);
+        {
+            using IndexSequence = std::make_index_sequence<sizeof...(Types)>;
+
+            [&]<size_t... I>(std::index_sequence<I...>) {
+                (visit(visitor, std::get<I>(value)), ...);
+            }(IndexSequence{});
         }
         visitor.leave(value);
     }
@@ -41,6 +79,15 @@ namespace slag {
     }
 
     template<typename Visitor, typename... Types>
+    constexpr inline void visit(Visitor&& visitor, std::variant<Types...>& value) {
+        visitor.enter(value);
+        std::visit([&](auto&& element) {
+            visit(visitor, element);
+        }, value);
+        visitor.leave(value);
+    }
+
+    template<typename Visitor, typename... Types>
     constexpr inline void visit(Visitor&& visitor, const std::variant<Types...>& value) {
         visitor.enter(value);
         std::visit([&](auto&& element) {
@@ -50,10 +97,28 @@ namespace slag {
     }
 
     template<typename Visitor, typename T>
+    constexpr inline void visit(Visitor&& visitor, std::vector<T>& value) {
+        visitor.enter(value);
+        for (auto&& element: value) {
+            visit(visitor, element);
+        }
+        visitor.leave(value);
+    }
+
+    template<typename Visitor, typename T>
     constexpr inline void visit(Visitor&& visitor, const std::vector<T>& value) {
         visitor.enter(value);
         for (auto&& element: value) {
             visit(visitor, element);
+        }
+        visitor.leave(value);
+    }
+
+    template<typename Visitor, typename Key, typename T>
+    constexpr inline void visit(Visitor&& visitor, std::unordered_map<Key, T>& value) {
+        visitor.enter(value);
+        for (auto&& element: value) {
+            visit(visitor, element); // visit the key/value pair
         }
         visitor.leave(value);
     }
