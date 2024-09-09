c59f3f3539a308933295fc326b9530b0f65c96d4
Joel Rausch <jsrausch.sojourn@gmail.com>
2024-09-08 21:28:17 -0500

    Allow resource types to be injected

--- src/slag/resource.h
+++ src/slag/resource.h
@@ -4,12 +4,22 @@
 
 #include <cstdint>
 
+#define SLAG_LIB_RESOURCE_TYPES(X) \
+    X(MANAGED)                     \
+    X(MESSAGE)                     \
+    X(BUFFER)                      \
+    X(FILE_DESCRIPTOR)             \
+    X(OPERATION)                   \
+
+// Resource types can be injected by defining this.
+// TODO: Think about how to finalize these.
+#ifndef SLAG_APP_RESOURCE_TYPES
+#  define SLAG_APP_RESOURCE_TYPES(X)
+#endif
+
 #define SLAG_RESOURCE_TYPES(X) \
-    X(MANAGED)                 \
-    X(MESSAGE)                 \
-    X(BUFFER)                  \
-    X(FILE_DESCRIPTOR)         \
-    X(OPERATION)               \
+    SLAG_LIB_RESOURCE_TYPES(X) \
+    SLAG_APP_RESOURCE_TYPES(X) \
 
 namespace slag {
 
@@ -38,5 +48,29 @@ namespace slag {
     using Buffer         = Resource<ResourceType::BUFFER>;
     using FileDescriptor = Resource<ResourceType::FILE_DESCRIPTOR>;
     using Operation      = Resource<ResourceType::OPERATION>;
+
+    constexpr bool is_lib_resource(ResourceType type) {
+        (void)type;
+
+#define X(SLAG_LIB_RESOURCE_TYPE)                                      \
+        if (type == ResourceType::SLAG_LIB_RESOURCE_TYPE) return true; \
+
+        SLAG_LIB_RESOURCE_TYPES(X)
+#undef X
+
+        return false;
+    }
+
+    constexpr bool is_app_resource(ResourceType type) {
+        (void)type;
+
+#define X(SLAG_APP_RESOURCE_TYPE)                                      \
+        if (type == ResourceType::SLAG_APP_RESOURCE_TYPE) return true; \
+
+        SLAG_APP_RESOURCE_TYPES(X)
+#undef X
+
+        return false;
+    }
 
 }
